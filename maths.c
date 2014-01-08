c5ec125e9c3ef97c8067d0eb5f613bda2fc018c4
Dan Foster <P11264278@GH582-010.lec-admin.dmu.ac.uk>
2014-01-08 10:42:33 +0000

    Add function

---
+++ maths.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("Enter a value for a: ");
+	scanf("%i", &a);
+	printf("Enter a value for b: ");
+	scanf("%i", &b);
+	c = a+b;
+	printf("A + B = %i\n",c);
+	return 0;
+}
